0da7b3c25a445724dd10acdd04252ebd9eaf4058
Qi19931106 <80775573+Qi19931106@users.noreply.github.com>
2021-12-10 16:21:55 +0800

    Update 1212(1).js

--- 1212(1).js
+++ 1212(1).js
@@ -15,7 +15,7 @@ if ($.isNode()) {
   cookiesArr = [$.getdata("CookieJD"), $.getdata("CookieJD2"), ...$.toObj($.getdata("CookiesJD") || "[]").map((item) => item.cookie)].filter((item) => !!item);
 }
 let cookie = "";
-$.code = "别看了就是这个，改这里！！！！！！！！！！！！！";
+$.code = "vKLxoyu";
 !(async () => {
   if (!cookiesArr[0]) {
     $.msg($.name, "【提示】请先获取京东账号一cookie\n直接使用NobyDa的京东签到获取", "https://bean.m.jd.com/bean/signIndex.action", { "open-url": "https://bean.m.jd.com/bean/signIndex.action" });
